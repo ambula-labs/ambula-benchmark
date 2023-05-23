897194318ffa63c4a3ac5bdd6d1f9d03c0cbdd67
intComodor <karim.bouali@etu.unistra.fr>
2023-05-23 23:52:21 +0200

    refactor script

--- src/monitor-logs.sh
+++ src/monitor-logs.sh
@@ -1,18 +1,16 @@
-#!/bin/bash
+LOG_DIR="log"
+LOG_FILES=("alice.log" "bob.log" "charlie.log" "dave.log" "eve.log" "ferdie.log")
 
-# this script is used to monitor the logs of the nodes and display a message when a reorg occurs
-
-for file in log/alice.log log/bob.log log/charlie.log log/dave.log log/eve.log log/ferdie.log
+for file in "${LOG_FILES[@]}"
 do
-    tail -f ${file} | while read -r LOGLINE
+    tail -f "${LOG_DIR}/${file}" | while read -r LOGLINE
     do
         if echo "${LOGLINE}" | grep --line-buffered "Reorg on #" > /dev/null; then
             BLOCK_NUMBER=$(echo ${LOGLINE} | perl -nle'print $& if m{(?<=Reorg on #)\d+}')
             TIMESTAMP=$(date "+[%Y-%m-%d %H:%M:%S]")
-            NODE_NAME=$(echo ${file} | cut -d'/' -f 2 | cut -d'.' -f 1)
+            NODE_NAME=$(echo ${file} | cut -d'.' -f 1)
             echo "${TIMESTAMP} [${NODE_NAME} node] Reorg on block number #${BLOCK_NUMBER}"
         fi
     done &
 done
 wait
-
