7e3ee3e67968a8f7652cc0c4f56aa94ead3d03e2
intComodor <karim.bouali@etu.unistra.fr>
2023-05-23 23:54:06 +0200

    refactor script

--- src/launch-nodes.sh
+++ src/launch-nodes.sh
@@ -2,85 +2,59 @@
 
 # this script is used to launch pre-configured nodes (Alice, Bob, Charlie, Dave, Eve, Ferdie) on the local network
 
+# path to the node template
+NODE_TEMPLATE="../../ambula/target/release/node-template"
+# predefined nodes
+NODES=("alice" "bob" "charlie" "dave" "eve" "ferdie")
+# alice node ports
+P2P_PORT=30333
+WS_PORT=9945
+RPC_PORT=9933
+# Alice node key
+ALICE_NODE_KEY="0000000000000000000000000000000000000000000000000000000000000001"
+# Alice bootstrap node
+BOOTSTRAP_NODE="/ip4/127.0.0.1/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp"
 
-../../ambula/target/release/node-template \
---base-path /tmp/alice \
---chain local \
---alice \
---port 30333 \
---ws-port 9945 \
---rpc-port 9933 \
---node-key 0000000000000000000000000000000000000000000000000000000000000001 \
---telemetry-url "wss://telemetry.polkadot.io/submit/ 0" \
---validator > log/alice.log 2>&1 &
+# loop over the nodes
+for NODE in ${NODES[@]}; do
+  if [ "$NODE" == "alice" ]; then
+    NODE_KEY="--node-key $ALICE_NODE_KEY"
+    BOOTSTRAP=""
+  else
+    NODE_KEY=""
+    BOOTSTRAP="--bootnodes $BOOTSTRAP_NODE"
+  fi
+  
+  $NODE_TEMPLATE \
+    --base-path /tmp/$NODE \
+    --chain local \
+    --$NODE \
+    --port $P2P_PORT \
+    --ws-port $WS_PORT \
+    --rpc-port $RPC_PORT \
+    --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" \
+    --validator \
+    $NODE_KEY \
+    $BOOTSTRAP \
+    > log/$NODE.log 2>&1 &
+  
+  echo "$NODE node launched"
 
-echo "Alice node launched"
-sleep 5
+  # incrementing the ports for the next node
+  P2P_PORT=$((P2P_PORT+1))
+  WS_PORT=$((WS_PORT+1))
+  RPC_PORT=$((RPC_PORT+1))
 
-../../ambula/target/release/node-template \
---base-path /tmp/bob \
---chain local \
---bob \
---port 30334 \
---ws-port 9946 \
---rpc-port 9934 \
---telemetry-url "wss://telemetry.polkadot.io/submit/ 0" \
---validator \
---bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp \
-> log/bob.log 2>&1 &
-echo "Bob node launched"
+  if [ "$NODE" == "alice" ]; then
+    sleep 5
+  fi
 
-../../ambula/target/release/node-template \
---base-path /tmp/charlie \
---chain local \
---charlie \
---port 30335 \
---ws-port 9947 \
---rpc-port 9935 \
---telemetry-url "wss://telemetry.polkadot.io/submit/ 0" \
---validator \
---bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp \
-> log/charlie.log 2>&1 &
-echo "Charlie node launched"
+  PIDS+=($!)
+done
 
-../../ambula/target/release/node-template \
---base-path /tmp/dave \
---chain local \
---dave \
---port 30336 \
---ws-port 9948 \
---rpc-port 9936 \
---telemetry-url "wss://telemetry.polkadot.io/submit/ 0" \
---validator \
---bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp \
-> log/dave.log 2>&1 &
-echo "Dave node launched"
+echo "All nodes launched."
 
-../../ambula/target/release/node-template \
---base-path /tmp/eve \
---chain local \
---eve \
---port 30337 \
---ws-port 9949 \
---rpc-port 9937 \
---telemetry-url "wss://telemetry.polkadot.io/submit/ 0" \
---validator \
---bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp \
-> log/eve.log 2>&1 &
-echo "Eve node launched"
+trap "echo 'Stopping nodes'; kill ${PIDS[*]}; exit" SIGINT SIGTERM
 
-../../ambula/target/release/node-template \
---base-path /tmp/ferdie \
---chain local \
---ferdie \
---port 30338 \
---ws-port 9950 \
---rpc-port 9938 \
---telemetry-url "wss://telemetry.polkadot.io/submit/ 0" \
---validator \
---bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp \
-> log/ferdie.log 2>&1
-echo "Ferdie node launched"
-
-echo "All nodes launched"
-
+# infinite loop to keep the script running
+while true; do sleep 1; done
